48a4183fa3b55674368fd91f8a103c5d337eacb0
Tomasz Cichocki <cichymail@gmail.com>
2019-04-23 21:55:24 +0200

    better mip

--- mip.py
+++ mip.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 import math
 import numpy as np
 from PIL import Image, ImageDraw
@@ -7,15 +7,12 @@ import random
 from pyscipopt import Model, quicksum
 import sys
 
-random.seed()
-
-SIZE = 5
-WIDTH = 1000
-HEIGHT = 1000
+WIDTH = 2000
+HEIGHT = 2000
 MIN_BASE_DIST = 1.0
 MAX_DIST = 0.005
 MAX_SOL_DIST = 0.001
-TIME_LIMIT = 3600
+TIME_LIMIT = 86400
 
 def connect(p1, p2, p3, p4):
     r1 = (p1[0]-p2[0])**2 + (p1[1]-p2[1])**2
@@ -106,37 +103,35 @@ def nextGeneration(previous):
 
     return ps
 
-# base_points - list of points from the base that are within delta to given points
-# base_points_mapping - maps base_points index to points index
 # new_points - range of new indexes
 # new_points_mapping - maps new_points index to points index
+# points_mapping - maps input points to base_points+new_points
 def filterPoints(base, points, delta):
     points_mapping = []
-    base_points = []
     new_points_mapping = []
 
     base = np.array(base)
-    new_idx = len(base)
+    new_idx = len(base) # first new_idx is just after base
     for i,point in enumerate(points):
         closest = np.argmin(np.sum(np.square(base - point), axis=1))
         closest_dist2 = np.sum(np.square(base[closest]-point))
         if closest_dist2 < delta*delta:
-            base_points.append(closest)
-            points_mapping.append(closest)
+            points_mapping.append(int(closest))
         else:
             new_points_mapping.append(i)
             points_mapping.append(new_idx)
             new_idx += 1
 
     new_points = range(len(base), new_idx)
-    return points_mapping, base_points, new_points, new_points_mapping
+    return points_mapping, new_points, new_points_mapping
 
 def findCandidates(base, points, delta):
     base = np.array(base)
+    delta2 = delta*delta
     for point in points:
         closest = np.argmin(np.sum(np.square(base - point), axis=1))
         closest_dist2 = np.sum(np.square(base[closest]-point))
-        if closest_dist2 < delta*delta:
+        if closest_dist2 < delta2:
             print(base[closest], closest_dist2)
             raise Exception("Point already in the base")
     # This is a list of arrays
@@ -178,7 +173,7 @@ def update(groupdict, edges, all_idxs):
             gs.append(hash)
         edges.append(gs)
 
-def makeDoc(sumgen, groupdict, edges):
+def makeDoc(sumgen, base_len, groupdict, edges, points_mapping):
     groupnum = dict()
     groups = []
     i = 0
@@ -191,43 +186,68 @@ def makeDoc(sumgen, groupdict, edges):
     groupnum = None
 
     doc = {}
-    doc["points"] = sumgen
-    doc["groups"] = groups
-    doc["edges"] = edges
+    doc["points"] = sumgen # list of all points coordinates
+    doc["numOfBasePoints"] = base_len # how many of them are base points
+    doc["groups"] = groups # all groups
+    doc["edges"] = edges # all edges
+    doc["points_mapping"] = points_mapping # how input points map to graph points
     return doc
 
-def buildMip(doc, target):
+def solveMip(doc):
+    target = doc["points_mapping"] # target are all input points mapped to graph ids
+
     m = Model()
     m.setRealParam('limits/time', TIME_LIMIT)
+
     gv = []
     for i in range(len(doc["groups"])):
         gv.append(m.addVar(f"g{i}", vtype='B'))
+
     pv = []
     for i in range(len(doc["points"])):
         pv.append(m.addVar(f"p{i}", vtype='B'))
+
     for i in target:
         m.addCons(pv[i] == 1)
+
     for i,ps in enumerate(doc["groups"]):
         # first point must be selected
         m.addCons(pv[ps[0]] >= gv[i])
         # and one of the following
         m.addCons(quicksum(pv[j] for j in ps[1:]) >= gv[i])
+
     for i,gs in enumerate(doc["edges"]):
         if len(gs) > 0:
             # at least two groups need to be selected
             m.addCons(quicksum(gv[j] for j in gs) >= 2*pv[i])
-    m.setObjective(quicksum(pv[i] for i in range(len(doc["points"]))), "minimize")
+
+    m.setObjective(
+        quicksum(
+            pv[i] for i in range(
+                doc["numOfBasePoints"], # don't pay for base points
+                len(doc["points"])
+            )
+        ),
+        "minimize")
+
     m.optimize()
+
     print("Input")
     print(target)
+
     optimized = []
+    bestObj = None
     for i,sol in enumerate(m.getSols()):
-        print("Solution")
         opt = []
-        for i,p in enumerate(pv):
+        for j,p in enumerate(pv):
             if m.getSolVal(sol,p) > 0:
-                opt.append(i)
-        print(len(opt))
+                opt.append(j)
+        obj = m.getSolObjVal(sol)
+        if bestObj == None:
+            bestObj = obj
+        print(f"Solution {i} (objective: {obj}, points: {len(opt)})")
+        if obj - bestObj <= 2:
+            print(json.dumps(opt))
         optimized.append(opt)
 
     return optimized[0]
@@ -247,21 +267,22 @@ def findBestCombination(p, ps, combinations):
     bestDist = float("inf")
     for c in combinations:
         pc = connect(ps[c[0]], ps[c[1]], ps[c[2]], ps[c[3]])
-        dist = euclid(p, pc)
-        if dist < bestDist:
-            bestC = c
-            bestDist = dist
+        if pc != None: # can be None when points are too close
+            dist = euclid(p, pc)
+            if dist < bestDist:
+                bestC = c
+                bestDist = dist
     if bestC == None:
         raise Exception(f"No combination for point {p}")
     return bestC, bestDist
 
-def makeRecipe(doc, base_len, point_list, target_list, delta):
+def makeRecipe(doc, point_list, delta):
     delta = 1.0 # don't be so strict
     points = doc["points"]
     groups = doc["groups"]
     edges  = doc["edges"]
+    base_len = doc["numOfBasePoints"]
     point_set = set(point_list)
-    target_set = set(target_list)
     # remove points not preset in solution from groups
     for i,g in enumerate(groups):
         if groups[i][0] not in point_set:
@@ -322,11 +343,10 @@ def draw(recipe, ps):
 
 # import pdb
 # pdb.set_trace()
-def main():
-    if len(sys.argv) < 2:
-        print("Give me problem file name!")
-        sys.exit(1)
-    input = readInput(sys.argv[1])
+def main(inputfn, basefn):
+    input = readInput(inputfn)
+    if basefn:
+        input['base']['points'] = readInput(basefn)
     MAX_GEN = input['base']['depth']
     base = input['base']['points']
 
@@ -346,7 +366,7 @@ def main():
 
     target = input["v"]
     # target += [random.choice(sumgen) for i in range(5)]
-    points_mapping, base_points, new_points, new_points_mapping = filterPoints(sumgen, target, MAX_DIST)
+    points_mapping, new_points, new_points_mapping = filterPoints(sumgen, target, MAX_DIST)
     print(points_mapping)
     print(new_points_mapping)
     new_points_coordinates = [ target[i] for i in new_points_mapping ]
@@ -357,12 +377,13 @@ def main():
     sumgen = sumgen + new_points_coordinates
 
     print("Making doc.")
-    doc = makeDoc(sumgen, groupdict, edges)
+    doc = makeDoc(sumgen, len(base), groupdict, edges, points_mapping)
     with open('graph.json', 'w') as outfile:
        json.dump(doc, outfile, separators=(',',':'))
+    # now doc and input should contain all that is needed to
 
     print("Building MIP.")
-    optimized = buildMip(doc, points_mapping)
+    optimized = solveMip(doc)
 
     out = Image.new("RGB", (WIDTH, HEIGHT), color=0)
     for pi in optimized:
@@ -370,9 +391,9 @@ def main():
         out.putpixel((int(p[0]), int(p[1])), (255,255,255) )
     out.save('original.png')
 
-    recipe = makeRecipe(doc, len(base), optimized, points_mapping, MAX_DIST)
+    recipe = makeRecipe(doc, optimized, MAX_DIST)
     #print(recipe)
-    out = normailzeRecipe(recipe, len(base), input["e"], points_mapping)
+    out = normailzeRecipe(recipe, doc["numOfBasePoints"], input["e"], doc["points_mapping"])
     allPoints = draw(out, base)
     out['coordinates'] = allPoints
     out['base'] = input['base']
@@ -381,4 +402,13 @@ def main():
         json.dump(out, outfile, separators=(',',':'))
 
 if __name__ == '__main__':
-    main()
+    if len(sys.argv) < 2:
+        print("Give me problem file name!")
+        sys.exit(1)
+
+    if len(sys.argv) == 3:
+        basefn = sys.argv[2]
+    else:
+        basefn = None
+
+    main(sys.argv[1], basefn)
